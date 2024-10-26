ea2ead177ea1ca82ae36b0a4391dff412760853f
Victor25-prog <pahecocazares89@gmail.com>
2024-10-25 20:48:41 -0600

    programa si

---
+++ EnvioGitHub/src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        String si = "", no = sc.nextLine();
+        if (si!="si"){
+            System.out.println("hola mundo");
+        }
+else{
+    System.out.println(" ");
+    
+
+        }
+    }
+}
